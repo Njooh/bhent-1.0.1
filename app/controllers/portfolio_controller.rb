b0052926e00bf07725ca981a88dcbb7491572130
Njooh <hello@njooh.com>
2016-12-19 09:30:16 +0100

    Bhent Website
    1.0.1

---
+++ app/controllers/portfolio_controller.rb
@@ -0,0 +1,39 @@
+class PortfolioController < ApplicationController
+
+  def index
+  end
+
+  def nyt_well
+  end
+
+  def authentic_weather
+  end
+
+  def bates_motel
+  end
+
+  def broacay_planet
+  end
+
+  def paloma_rincon
+  end
+
+  def france_cisco
+  end
+
+  def five_dime
+  end
+
+  def soul_plasticity
+  end
+
+  def recipes
+  end
+
+  def wrist_watch
+  end
+
+  def forpackad
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,41 @@
+Rails.application.routes.draw do
+
+  get 'blog' => 'blog#index'
+
+  get 'blog/article/1' => 'blog#article_1'
+
+  get 'portfolio/bates-motel' => 'portfolio#bates_motel'
+
+  get 'portfolio/broacay-planet' => 'portfolio#broacay_planet'
+
+  get 'portfolio/paloma-rincon' => 'portfolio#paloma_rincon'
+
+  get 'portfolio/france-cisco' => 'portfolio#france_cisco'
+
+  get 'portfolio/five-dime' => 'portfolio#five_dime'
+
+  get 'portfolio/soul-plasticity' => 'portfolio#soul_plasticity'
+
+  get 'portfolio/recipes' => 'portfolio#recipes'
+
+  get 'portfolio/wrist-watch' => 'portfolio#wrist_watch'
+
+  get 'portfolio/forpackad' => 'portfolio#forpackad'
+
+  get 'portfolio/the-deer-hunter' => 'portfolio#the_deer_hunter'
+
+  get 'portfolio/authentic-weather' => 'portfolio#authentic_weather'
+
+  get 'portfolio/nyt-well' => 'portfolio#nyt_well'
+
+  get 'portfolio' => 'portfolio#index'
+
+  get 'about-us-agency' => 'home#about_us'
+
+  root 'home#index'
+
+  get 'contact-us' => 'contact_us#index'
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
